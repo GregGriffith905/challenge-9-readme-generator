f069c50652fee7b65a882b56741cb6ac88a3f2a0
Greg Griffith <griffith_g@hotmail.com>
2022-12-09 03:34:11 -0500

    added questions section, started on license badges and links

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
-// TODO: Include packages needed for this application
+// TODO: Include packages needed for this application 
 const inquirer = require('inquirer');        //CLI interface for node.js
 const fs = require('fs');                    //file system module              
+
 
 // TODO: Create an array of questions for user input
 const questions = {
@@ -16,33 +17,45 @@ const questions = {
     getBadges: "Badges aren't necessary, per se, but they demonstrate street cred.\n  Badges let other developers know that you know what you're doing.\n  Check out the badges hosted by [shields.io](https://shields.io/).\n  You may not understand what they all represent now, but you will in time.\n>",
     getFeatures: "If the project has a lot of features, list them here.\n>",
     getContributions: "If other developers are allowed to contribute to this application, include guidelines for how to do so.\n  The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard,\n  but you can always write your own if you'd prefer.\n>",
-    getTests: "Go the extra mile and write tests for this application. Then provide examples on how to run them here.\n>"
+    getTests: "Go the extra mile and write tests for this application. Then provide examples on how to run them here.\n>",
+    getQuestions: "Add repository and contact info to README?\n>",
+    getGithub: "Enter your GitHub username: ",
+    getEmail: "Enter your E-mail address: ",
 }
-const generateTOC = (install,usage,credits,license,badges,features,contributions,tests) => //generate table of contents  
-    `${`## Table of Contents\n`}`+
-    `${install? `\n - [Installation](#install)`:``}`+     //only include items in TOC if user responded to question
-    `${usage? `\n - [Usage](#usage)`:``}`+                //eg if usage was answered then include usage in TOC
-    `${credits? `\n - [Credits](#credits)`:``}`+ 
-    `${license? `\n - [License](#license)`:``}`+ 
-    `${badges? `\n - [Badges](#badges)`:``}`+ 
-    `${features? `\n - [Features](#features)`:``}`+ 
-    `${contributions? `\n - [Contributions](#contributions)`:``}`+ 
-    `${tests? `\n - [Tests](#tests)`:``}`;
+const generateTOC = (install,usage,credits,license,badges,features,contributions,tests,github,email) => //generate table of contents  
+    `${`## Table of Contents\n\n`}`+
+    `${install? ` - [Installation](#install)\n`:``}`+     //only include items in TOC if user responded to question
+    `${usage? ` - [Usage](#usage)\n`:``}`+                //eg if usage was answered then include usage in TOC
+    `${credits? ` - [Credits](#credits)\n`:``}`+ 
+    `${license? ` - [License](#license)\n`:``}`+ 
+    `${badges? ` - [Badges](#badges)\n`:``}`+ 
+    `${features? ` - [Features](#features)\n`:``}`+ 
+    `${contributions? ` - [Contributions](#contributions)\n`:``}`+ 
+    `${tests? ` - [Tests](#tests)\n`:``}`+  
+    `${github||email? ` - [Questions](#questions)\n`:``}`;
  
-const createReadMe = ({title,description,includeTOC,install,usage,credits,license,badges,features,contributions,tests})=>
+const generateQues = (github,email) => //generate the questions section of the README
+    `${github||email? `## Questions\n\n`:``}`+                            //display questions setion if any content
+    `${github? `GitHub: [${github}](https://github.com/${github}) \n\n`:``}`+  //display github... 
+    `${email? `E-Mail: [${email}](mailto:${email})\n\n`:``}`;                  //display email...  
+  
+const createReadMe = ({title,description,includeTOC,install,usage,credits,license,badges,features,contributions,tests,questions,github,email})=>
 // creates README content, do not include titles of empty sections in final README, calls generateTOC   
-    `${`# ${title.toUpperCase()}\n\n`}`+
+    `${`# ${title.toUpperCase()}\n\n`}`+                                  //always show title
+    `${license? renderLicenseBadge(license):``}`+                         //display license badge if any selected  
+    `${license? renderLicenseLink(license):``}`+                          //display license link...  
     `${`## Description\n\n${description}\n\n`}`+                          //always show description header              
-    `${includeTOC? generateTOC(install,usage,credits,license,badges,features,contributions,tests)+`\n\n` :``}`+ //generateTOC function
+    `${includeTOC? generateTOC(install,usage,credits,license,badges,features,contributions,tests,github,email)+`\n` :``}`+ //generateTOC function
     `${install? `## Installation\n\n${install}\n\n`:``}`+                 //display install section if any content  
     `${usage? `## Usage\n\n${usage}\n\n`:``}`+                            //display usage section if any content
     `${credits? `## Credits\n\n${credits}\n\n`:``}`+                      //display credits...  
     `${license? `## License\n\n${license}\n\n`:``}`+                      //display license...
     `${badges? `## Badges\n\n${badges}\n\n`:``}`+                         //display badges...
     `${features? `## Features\n\n${features}\n\n`:``}`+                   //display features...  
-    `${contributions? `## Contributions\n\n${contributions}\n\n`:``}`+    //display contributions
-    `${tests? `## Tests\n\n${tests}\n\n`:``}`;                            //display tests              
-
+    `${contributions? `## Contributions\n\n${contributions}\n\n`:``}`+    //display contributions...
+    `${tests? `## Tests\n\n${tests}\n\n`:``}`+                            //display tests...              
+    `${questions? generateQues(github,email)+`\n\n`:``}`;                 //display contact info...
+    
 function isChecked(array,value){    //checks if checkbox was selected
     var isItIn = false;
     if (array) array.forEach (element => {if (element==value) isItIn = true}); //check every element in array to see if ==value
@@ -50,7 +63,9 @@ function isChecked(array,value){    //checks if checkbox was selected
     }
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  //  fs.write(filename,process.argv,err=>)
+}
 
 // TODO: Create a function to initialize app
  function init() {
@@ -67,8 +82,8 @@ function writeToFile(fileName, data) {}
       name: 'description',
     },
     {
-      type: 'confirm',                           //ask user if table of contents should be included
-      message: questions.getTOC,
+      type: 'confirm',                           //(Y/n)
+      message: questions.getTOC,                 //ask user if table of contents should be included
       name: 'includeTOC',      
     },
     {
@@ -87,17 +102,18 @@ function writeToFile(fileName, data) {}
       name: 'credits',
     },
     {
-      type: 'input',
+      type: 'list',
       message: questions.getLicense,             //ask user about licenses
+      choices : ["none","MIT License","GNU AGPLv3","GNU GPLv3","GNU LGPLv3","Boost Software License 1.0","Apache License 2.0","Mozilla Public License","The Unlicensed"],
       name: 'license',
     },
     {
-      type: 'confirm',
+      type: 'confirm',                           //(Y/n)
       message: questions.getDetails,             //ask user if to stop readme now or ask more questions
       name: 'details',
     },
     {
-      type: 'checkbox',
+      type: 'checkbox',                                                   //checkbox, can select multiple choices              
       message: questions.getDetailList,                                   //ask user to select choices from list
       choices: ["Badges","Features","Contributions","Tests"],
       name: 'detailList',
@@ -127,6 +143,25 @@ function writeToFile(fileName, data) {}
       name: 'tests',
       when:  response => isChecked(response.detailList,"Tests"),          //only ask this question if Tests was checked
     },
+    
+    {
+      type: 'confirm',
+      message: questions.getQuestions,                                    //ask user how to include contact into
+      name: 'questions',
+    },
+    {
+      type: 'input',
+      message: questions. getGithub,                                      //ask user for github account
+      name: 'github',
+      when:  response =>response.questions == true,                       //only ask this question if selected 'Y' for contact info
+    },
+    {
+      type: 'input',
+      message: questions.getEmail,                                        //ask user for email address
+      name: 'email',
+      when:  response => response.questions == true,                      //only ask this question if selected 'Y' for contact info
+    },
+
   ])
   .then((response) =>{
     const readMeDoc = createReadMe(response);       //generate README
@@ -134,9 +169,8 @@ function writeToFile(fileName, data) {}
     console.log(readMeDoc); 
     console.log("The following information was written to README file:");
     console.log(response);
-  });
-
+  })
+  .catch(() => console.log("Oops, Something went wrong!"));     //error message to return on error
  }
-
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,10 +1,36 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// function renderLicenseBadge(license) {
+//   switch(license){
+//     case "none": return ''
+//     case "MIT License": return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;break;
+//     case "GNU AGPLv3": return `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`;break;
+//     case "GNU GPLv3": return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;break;
+//     case "GNU LGPLv3": return `[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)`;break;
+//     case "Boost Software License 1.0": return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`;break;
+//     case "Apache License 2.0": return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;break;
+//     case "Mozilla Public License 2.0": return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;break;
+//     case "The Unlicense": return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`;break;
+//   }
+// }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {
+//   function renderLicenseBadge(license) {
+//     switch(license){
+//       case "none": return ''
+//       case "MIT License": return `[![License: MIT](https://opensource.org/licenses/MIT)`;break;
+//       case "GNU AGPLv3": return `[![License: AGPL v3](https://www.gnu.org/licenses/agpl-3.0)`;break;
+//       case "GNU GPLv3": return `[![License: GPL v3](https://www.gnu.org/licenses/gpl-3.0)`;break;
+//       case "GNU LGPLv3": return `[![License: LGPL v3](https://www.gnu.org/licenses/lgpl-3.0)`;break;
+//       case "Boost Software License 1.0": return `[![License](https://www.boost.org/LICENSE_1_0.txt)`;break;
+//       case "Apache License 2.0": return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;break;
+//       case "Mozilla Public License 2.0": return `[![License: MPL 2.0](https://opensource.org/licenses/MPL-2.0)`;break;
+//       case "The Unlicense": return `[![License: Unlicense](http://unlicense.org/)`;break;
+//     }
+//   }
+// }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -14,7 +40,11 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  //then badge 
+  //then link
+
 `;
 }
 
 module.exports = generateMarkdown;
+ 
